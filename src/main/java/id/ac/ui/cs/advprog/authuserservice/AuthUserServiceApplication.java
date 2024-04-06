20317141f4e460693d47e3391d245c1054a306ff
Dwiky Ahmad Megananta <119550129+DwikyAhmad@users.noreply.github.com>
2024-04-06 12:05:53 +0700

    first commit

---
+++ src/main/java/id/ac/ui/cs/advprog/authuserservice/AuthUserServiceApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.authuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthUserServiceApplication.class, args);
+    }
+
+}
