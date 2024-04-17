4e86433e28e9786d07623f49c1506be832e42f1a
kimiagastya <kimiagastya@gmail.com>
2024-04-17 16:24:26 +0700

    Change from @Controller to @RestController

--- src/main/java/id/ac/ui/cs/advprog/authuserservice/controller/HelloController.java
+++ src/main/java/id/ac/ui/cs/advprog/authuserservice/controller/HelloController.java
@@ -1,9 +1,9 @@
 package id.ac.ui.cs.advprog.authuserservice.controller;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloController {
     @GetMapping("/")
     public String helloPage(Model model){
