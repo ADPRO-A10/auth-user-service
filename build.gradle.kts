3b404a33908b6cc3d5e7eb9eaca2fa1b6838ad0c
kimiagastya <kimiagastya@gmail.com>
2024-05-26 23:14:55 +0700

    Fix version for sonar

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     id("org.springframework.boot") version "3.2.4"
     id("io.spring.dependency-management") version "1.1.4"
     jacoco
-    id("org.sonarqube") version "4.4.1.3373"
+    id("org.sonarqube") version "4.0.0.2929"
 }
 
 sonarqube {
